4402c561616f43a210466b6961136a6343cd833e
Diana Tukenova <d.tukenova@gmail.com>
2019-02-10 16:59:33 +0600

    fb

---
+++ week1/codeacademy JS/farenheit.js
@@ -0,0 +1,6 @@
+const kelvin = 0; //hz
+const celsius = kelvin - 273; //cs
+let farenheit = celsius * (9/5) + 32; //dsd
+farenheit = Math.floor(farenheit);
+console.log(`The temperature is ${farenheit} degrees Fahrenheit.`);
+
